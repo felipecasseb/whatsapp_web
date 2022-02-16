71660ddc153779d9cf6459acb5bd447afd7b4bd3
Felipe Casseb <felipe.casseb@hotmail.com>
2022-02-15 21:16:58 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/felipecasseb/whatsapp_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.felipecasseb.whatsapp_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_web/screens/login_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Whatsapp Web",
+    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
