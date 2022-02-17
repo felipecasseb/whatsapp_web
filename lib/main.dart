1cb38a7498911430242cdd8f387b508803e6f0f4
Felipe Casseb <felipe.casseb@hotmail.com>
2022-02-17 11:34:55 -0300

    Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:whatsapp_web/screens/login_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Whatsapp Web",
+    title: "Whatsapp Web teste",
     home: LoginScreen(),
     debugShowCheckedModeBanner: false,
   ));
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,8 +8,131 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _controllerNome = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+
+  bool _cadastroUsuario = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double altura = MediaQuery.of(context).size.height;
+    double largura = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        color: Color(0XFFD9DBD5),
+        width: largura,
+        height: altura,
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                width: largura,
+                height: altura * 0.5,
+                color: Color(0xFF075E54),
+              ),
+            ),
+            Center(
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Container(
+                    padding: EdgeInsets.all(40),
+                    width: 500,
+                    child: Column(
+                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Visibility(
+                          visible: _cadastroUsuario,
+                          child: ClipOval(
+                              child: Image.asset(
+                            "images/perfil.png",
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          )),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Visibility(
+                          visible: _cadastroUsuario,
+                          child: OutlinedButton(
+                              onPressed: () {}, child: Text("Selecionar foto")),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Visibility(
+                          visible: _cadastroUsuario,
+                          child: TextField(
+                            controller: _controllerNome,
+                            decoration: InputDecoration(
+                                hintText: "Nome",
+                                labelText: "Nome",
+                                suffixIcon: Icon(Icons.person_outline)),
+                          ),
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _controllerEmail,
+                          decoration: InputDecoration(
+                              hintText: "Email",
+                              labelText: "Email",
+                              suffixIcon: Icon(Icons.email_outlined)),
+                        ),
+                        TextField(
+                          obscureText: true,
+                          controller: _controllerSenha,
+                          decoration: InputDecoration(
+                              hintText: "Senha",
+                              labelText: "Senha",
+                              suffixIcon: Icon(Icons.lock_outline)),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  primary: Color(0xFF075E54)),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8),
+                                child: Text(
+                                  _cadastroUsuario ? "Cadastrar" : "Entrar",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              )),
+                        ),
+                        Row(
+                          children: [
+                            Text("Login"),
+                            Switch(
+                                value: _cadastroUsuario,
+                                onChanged: (bool valor) {
+                                  setState(() {
+                                    _cadastroUsuario = valor;
+                                  });
+                                }),
+                            Text("Cadastro"),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
